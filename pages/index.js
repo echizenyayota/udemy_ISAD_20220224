0d225f483ccacd4672a0afb8cd8373c5786e48e5
echizenyayota <virtualmisso30@gmail.com>
2022-03-01 19:11:23 +0900

    15. Products Button Setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
+import React, { useState } from 'react';
 import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { ResourcePicker } from '@shopify/app-bridge-react';
 
 function Index() {
+
+  const [modal, setModal] = useState({ open: false });
+
   return(
     <Page>
+      <ResourcePicker
+        resourceType="Product"
+        showVariants={false}
+        open={modal.open}
+        onCancel={ () => setModal({ open: false })}
+      />
       <Layout>
         <EmptyState
           heading="Manage your inventory transfers"
-          action={{content: 'Select Products'}}
+          action={{
+            content: 'Select Products',
+            onAction: () => setModal({ open: true })
+          }}
           image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
         >
           <p>Select Products</p>
