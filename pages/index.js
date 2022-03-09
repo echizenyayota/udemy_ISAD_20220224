fb4e96f5c118cdd5ade66168f39f4337ab78b59a
echizenyayota <virtualmisso30@gmail.com>
2022-03-09 16:11:20 +0900

    18. Save Product ID's

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker } from '@shopify/app-bridge-react';
+import store from 'store-js';
 
 function Index() {
 
   const [modal, setModal] = useState({ open: false });
+  const emptyState = !store.get('ids');
+  
+  function handleSection(resources) {
+    const idsFromResources = resources.selection.map((product) => product.id);
+    setModal({ open: false });
+    store.set('ids', idsFromResources)
+    console.log('this is product id', store.get('ids'));
+  }
 
   return(
     <Page>
@@ -13,6 +22,7 @@ function Index() {
         showVariants={false}
         open={modal.open}
         onCancel={ () => setModal({ open: false })}
+        onSelection={(resources) => handleSection(resources)}
       />
       <Layout>
         <EmptyState
